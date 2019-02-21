49da27b4610b8dd1db9421e428852285debcb432
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-02-21 16:17:48 -0300

    tidy up PageThreadShow component extracting in into smaller components; use of computed properties and add posts counts per user

---
+++ src/components/PostListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="post">
+    <div class="user-info">
+      <a href="#" class="user-name">{{ user.name }}</a>
+
+      <a href="#">
+        <img :src="user.avatar" alt class="avatar-large">
+      </a>
+
+      <p class="desktop-only text-small">{{ userPostsCount }} posts</p>
+    </div>
+
+    <div class="post-content">
+      <div>{{ post.text }}</div>
+    </div>
+
+    <div class="post-date text-faded">{{ post.publishedAt }}</div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+
+export default {
+  props: {
+    post: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    user () {
+      return sourceData.users[this.post.userId]
+    },
+    userPostsCount () {
+      const postIds = this.user.posts
+      const postIdsArray = Object.keys(postIds)
+      return postIdsArray.length
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,32 +2,18 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
 
-    <div class="post-list">
-      <div v-for="postId in thread.posts" class="post">
-        <div class="user-info">
-          <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
-
-          <a href="#">
-            <img :src="users[posts[postId].userId].avatar" alt class="avatar-large">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>{{ posts[postId].text }}</div>
-        </div>
-
-        <div class="post-date text-faded">{{ posts[postId].publishedAt }}</div>
-      </div>
-    </div>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import PostList from '@/components/PostList'
 
 export default {
+  components: {
+    PostList
+  },
   props: {
     id: {
       required: true,
@@ -36,9 +22,14 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      thread: sourceData.threads[this.id]
+    }
+  },
+  computed: {
+    posts () {
+      const postIds = Object.values(this.thread.posts)
+      return Object.values(sourceData.posts)
+        .filter(post => postIds.includes(post['.key']))
     }
   }
 }
