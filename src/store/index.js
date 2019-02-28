cbb3bb7f6614d0066dae1a840a82103a222d2871
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-02-28 17:48:27 -0300

    fetching data before creating the component instance because we dont need to have reactivity to dispatch an action, we dont have an id received so we need all the forums created and the categories

--- src/store/index.js
+++ src/store/index.js
@@ -129,8 +129,26 @@ export default new Vuex.Store({
       return dispatch('fetchItem', {resource: 'posts', id, emoji: '💬‍'})
     },
 
+    fetchForums ({dispatch}, {ids}) {
+      return dispatch('fetchItems', {resource: 'forums', emoji: '🌧', ids})
+    },
+
     fetchPosts ({dispatch}, {ids}) {
       return dispatch('fetchItems', {resource: 'posts', emoji: '💬‍', ids})
+    },
+
+    fetchAllCategories ({state, commit}) {
+      console.log('🔥', '🏷', 'all')
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('categories').once('value', snapshot => {
+          const categoriesObject = snapshot.val()
+          Object.keys(categoriesObject).forEach(categoryId => {
+            const category = categoriesObject[categoryId]
+            commit('setItem', {resource: 'categories', id: categoryId, item: category})
+          })
+          resolve(Object.values(state.categories))
+        })
+      })
     },
 
     fetchItem ({state, commit}, {id, emoji, resource}) {
