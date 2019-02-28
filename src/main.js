1db95dcd20f502dbe847ed64f384fc71602b77d2
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-02-28 15:00:34 -0300

    add firebase dependency; connect app to firebase in main.js; load data.json to realtime firebase to do that I need to remove all .key properties bc conflicted with firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import AppDate from '@/components/AppDate'
@@ -9,6 +10,12 @@ import store from '@/store'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const config = {
+  // firebase web keys config
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
