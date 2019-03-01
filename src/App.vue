810f22034ad4a18d6f07509a5dc312f607e50b40
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-03-01 15:54:51 -0300

    using NProgress to show a progress bar while loading pages

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <TheNavbar />
     <div class="container">
-      <router-view v-show="showPage" @ready="showPage = true"/>
+      <router-view v-show="showPage" @ready="pageReady"/>
       <AppSpinner v-show="!showPage"></AppSpinner>
     </div>
   </div>
@@ -11,10 +11,17 @@
 <script>
 import TheNavbar from '@/components/TheNavbar'
 import AppSpinner from '@/components/AppSpinner'
+import NProgress from 'nprogress'
 export default {
   components: {
     TheNavbar,
     AppSpinner
+  },
+  methods: {
+    pageReady () {
+      this.showPage = true
+      NProgress.done()
+    }
   },
   data () {
     return {
@@ -22,8 +29,14 @@ export default {
     }
   },
   created () {
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false
+    })
+    NProgress.start()
     this.$router.beforeEach((to, from, next) => {
       this.showPage = false
+      NProgress.start()
       next()
     })
   }
@@ -33,5 +46,9 @@ export default {
 <style>
 
   @import "assets/css/style.css";
+  @import "~nprogress/nprogress.css"; // ~ look in the node_modules
 
+  #nprogress .bar {
+    background: #57AD8D;
+  }
 </style>
