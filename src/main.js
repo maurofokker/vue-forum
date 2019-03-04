880aee4981e74b74fb00dcb6800f7ee897a89d32
maurofokker <mgaldames@gmail.com>
2019-03-03 23:36:47 -0300

    add dotenv to keep configdata secret

--- src/main.js
+++ src/main.js
@@ -14,7 +14,12 @@ Vue.config.productionTip = false
 // Initialize Firebase
 const config = {
   // firebase web keys config
-
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 firebase.initializeApp(config)
 
