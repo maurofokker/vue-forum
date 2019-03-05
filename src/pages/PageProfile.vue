58feb80795e6f5974db0aec10616933159c1830f
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-03-05 11:55:22 -0300

    clean of the profile page; using getter to fetch actual posts created by the user

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -1,24 +1,23 @@
 <template>
   <div class="flex-grid">
-    <h1>My Profile</h1>
-    <!--<UserProfileCard-->
-    <!--v-if="!edit"-->
-      <!--:user="user" />-->
+    <UserProfileCard
+      v-if="!edit"
+      :user="user" />
 
-    <!--<UserProfileCardEditor-->
-      <!--v-else-->
-      <!--:user="user" />-->
+    <UserProfileCardEditor
+      v-else
+      :user="user" />
 
-    <!--<div class="col-7 push-top">-->
-      <!--<div class="profile-header">-->
-        <!--<span class="text-lead">{{user.username}} recent activity</span>-->
-        <!--<a href="#">See only started threads?</a>-->
-      <!--</div>-->
+    <div class="col-7 push-top">
+      <div class="profile-header">
+        <span class="text-lead">{{user.username}} recent activity</span>
+        <a href="#">See only started threads?</a>
+      </div>
 
-      <!--<hr>-->
+      <hr>
 
-      <!--<PostList :posts="userPosts" />-->
-    <!--</div>-->
+      <PostList :posts="userPosts" />
+    </div>
   </div>
 </template>
 
@@ -27,6 +26,7 @@ import PostList from '@/components/PostList'
 import UserProfileCard from '@/components/UserProfileCard'
 import UserProfileCardEditor from '@/components/UserProfileCardEditor'
 import { mapGetters } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 
 export default {
 
@@ -35,6 +35,8 @@ export default {
     UserProfileCard,
     UserProfileCardEditor
   },
+
+  mixins: [asyncDataStatus],
 
   props: {
     edit: {
@@ -49,16 +51,14 @@ export default {
     }),
 
     userPosts () {
-      if (this.user.posts) {
-        return Object.values(this.$store.state.posts)
-          .filter(post => post.userId === this.user['.key'])
-      }
-      return []
+      return this.$store.getters.userPosts(this.user['.key'])
     }
   },
 
   created () {
-    this.$emit('ready')
+    // fetch the user posts before emit the ready event
+    this.$store.dispatch('fetchPosts', {ids: this.user.posts})
+      .then(() => this.asyncDataStatus_fetched())
   }
 
 }
