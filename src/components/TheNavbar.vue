bd5dcfa0cfd921606b0c6a17b482f79c03ad1dda
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-03-07 15:18:59 -0300

    custom directive to close navbar on scroll support for mobile devices too

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="header" id="header" v-click-outside="closeMobileNavbar">
+  <header class="header" id="header" v-click-outside="closeMobileNavbar" v-handle-scroll="closeMobileNavbar">
     <router-link
       :to="{name: 'Home'}"
       class="logo"
@@ -63,10 +63,12 @@
 <script>
 import { mapGetters } from 'vuex'
 import clickOutside from '@/directives/click-outside'
+import handleScroll from '@/directives/handle-scroll'
 
 export default {
   directives: {
-    clickOutside
+    clickOutside,
+    handleScroll
   },
 
   data () {
