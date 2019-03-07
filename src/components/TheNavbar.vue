26139d7a57951ffc380691c30785880865d170cd
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-03-07 15:10:41 -0300

    setting the viewport so the design becomes responsive and make the navbar works on mobile devices

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="header" id="header">
+  <header class="header" id="header" v-click-outside="closeMobileNavbar">
     <router-link
       :to="{name: 'Home'}"
       class="logo"
@@ -7,7 +7,7 @@
       <img src="../assets/img/vueschool-logo.svg">
     </router-link>
 
-    <div class="btn-hamburger">
+    <div class="btn-hamburger" @click="mobileNavOpen = !mobileNavOpen">
       <!-- use .btn-humburger-active to open the menu -->
       <div class="top bar"></div>
       <div class="middle bar"></div>
@@ -15,7 +15,7 @@
     </div>
 
     <!-- use .navbar-open to open nav -->
-    <nav class="navbar">
+    <nav class="navbar" :class="{'navbar-open': mobileNavOpen}">
       <ul v-if="user">
         <li class="navbar-user" v-click-outside="closeUserDropdown">
           <a @click.prevent="userDropdownOpen = !userDropdownOpen">
@@ -44,6 +44,8 @@
             </ul>
           </div>
         </li>
+        <li class="navbar-mobile-item"><router-link :to="{name: 'Profile'}">View Profile</router-link></li>
+        <li class="navbar-mobile-item"><a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a></li>
       </ul>
 
       <ul v-else>
@@ -69,7 +71,8 @@ export default {
 
   data () {
     return {
-      userDropdownOpen: false
+      userDropdownOpen: false,
+      mobileNavOpen: false
     }
   },
 
@@ -82,6 +85,9 @@ export default {
   methods: {
     closeUserDropdown () {
       this.userDropdownOpen = false
+    },
+    closeMobileNavbar () {
+      this.mobileNavOpen = false
     }
   }
 
